2151c4c849e2b016252dd4e11deccc6ae1c1b233
KartikVashisth97 <kartikvashisth675@gmail.com>
2018-12-05 17:34:49 -0500

    init: initial project

---
+++ Rummikub/src/main/java/TileRummy/Rummikub/App.java
@@ -0,0 +1,13 @@
+package TileRummy.Rummikub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
